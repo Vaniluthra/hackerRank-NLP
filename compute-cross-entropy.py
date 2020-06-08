a0aed0bfd3f753f2c00bb83ff4c717da2adc210f
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-06-08 18:57:43 -0400

    initial commit

---
+++ compute-cross-entropy.py
@@ -0,0 +1,5 @@
+#cross_entropy = log(perplexity)
+import math
+perplexity = 170
+cross_entropy = math.log2(170)
+print(round(cross_entropy,2))
---
+++ compute-perplexity.py
@@ -0,0 +1,5 @@
+#perplexity = 2^entropy
+import math
+entropy = 9.91
+perplexity = 2**9.91
+print(int(perplexity))
